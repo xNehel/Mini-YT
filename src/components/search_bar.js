ce9b274256abf4a8295cdbb600857d4d97158e1d
xNehel <eskrzy@gmail.com>
2016-12-22 21:54:21 +0100

    Added state to search to bind user input into video list

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -7,17 +7,17 @@ class SearchBar extends Component {
   }
   render() {
     return (
-        <div>
+        <div className="search-bar">
           <input
               value={this.state.term}
-              onChange={event=>this.setState({term: event.target.value})} />
-          Value ofthe input: {this.state.term}
+              onChange={event => this.onInputChange(event.target.value)} />
         </div>
     );
   }
 
-  onInputChange(event){
-    console.log(event.target.value);
+  onInputChange(term){
+    this.setState({term});
+    this.props.onSearchTermChange(term);
   }
 }
 
